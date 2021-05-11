8b67b3e30717194524fc3f0782ae690c0326ac69
ilove3q <ilove3q@gmail.com>
2021-05-11 17:58:19 +0200

    Initial commit

---
+++ src/main/java/com/crm/gestionstock/CrmGestionStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.crm.gestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmGestionStockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmGestionStockApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crm/gestionstock/CrmGestionStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.gestionstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmGestionStockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
