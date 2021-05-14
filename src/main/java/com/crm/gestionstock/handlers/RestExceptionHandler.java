27ae0f67243f251b8abc47b4a9c8b7d74ed972d3
ilove3q <ilove3q@gmail.com>
2021-05-14 18:21:00 +0200

    update handler exception

---
+++ src/main/java/com/crm/gestionstock/handlers/RestExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.crm.gestionstock.handlers;
+
+import com.crm.gestionstock.exception.EntityNotFoundException;
+import com.crm.gestionstock.exception.ErrorCodes;
+import com.crm.gestionstock.exception.InvalidEntityException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.awt.dnd.InvalidDnDOperationException;
+import java.util.Collections;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleException(EntityNotFoundException exception, WebRequest webRequest) {
+
+        final HttpStatus notFound = HttpStatus.NOT_FOUND;
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(notFound.value())
+                .message(exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorDto, notFound);
+    }
+
+    @ExceptionHandler(InvalidEntityException.class)
+    public ResponseEntity<ErrorDto> handleException(InvalidEntityException exception, WebRequest webRequest) {
+        final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(badRequest.value())
+                .message(exception.getMessage())
+                .errors(exception.getErrors())
+                .build();
+
+        return new ResponseEntity<>(errorDto, badRequest);
+    }
+
+}
